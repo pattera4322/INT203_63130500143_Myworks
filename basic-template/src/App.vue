e18fa3efb7fd4259904fe1a91e34abae6960125e
Munmint <68814056+pattera4322@users.noreply.github.com>
2022-01-25 11:30:11 +0700

    Add files via upload

---
+++ basic-template/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  const msg = 'Hello Kitty'
+  const headingStyle = 'color: blue'
+  const paraStyle = 'text-decoration:underline'
+  const imagePath = '../public/images/eiei.jpg'
+</script>
+ 
+<template>
+  <h1 v-bind:style="headingStyle">INT203 Web Client II</h1>
+  <h2>{{ msg }}</h2>
+  <p :style="paraStyle">lecture</p>
+  <img src="./assets/logo.png" />
+  <img :src="imagePath" alt="eiei" srcset="" />
+</template>
+ 
+<style>
+
+</style>
